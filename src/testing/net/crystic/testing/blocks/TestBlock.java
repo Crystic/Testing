49ecd8724403d7332c972116d0ca7d19f0d7cabc
Crystic <crysticdev@gmail.com>
2014-07-28 01:14:37 +0100

    TestBlock added
    Added test block and TestBlock.class

---
+++ src/testing/net/crystic/testing/blocks/TestBlock.java
@@ -0,0 +1,28 @@
+package net.crystic.testing.blocks;
+
+import net.crystic.testing.Testing;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class TestBlock extends Block{
+
+	public TestBlock(Material material) {
+		super(material);
+		
+		this.setHardness(3.0F);
+		this.setResistance(5.0F);
+		this.setStepSound(soundTypeStone);
+		this.setCreativeTab(getCreativeTabToDisplayOn().tabBlock);
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcon(IIconRegister iconRegister){
+		
+		this.blockIcon = iconRegister.registerIcon(Testing.modid + ":" + this.getUnlocalizedName().substring(5));
+		
+	}
+
+}
